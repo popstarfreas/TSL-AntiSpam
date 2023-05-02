1196aeee35d12fe1cd0710ed3793aa98da096c6c
Glasia <132226783+GlasiaVD23@users.noreply.github.com>
2023-05-02 20:52:48 +0200

    Small correction (behavior of trimming same letter spam)
    Corrected the trimming behavior of too many repeated letters: now should trim texts like "Nooooo!!" to "Nooo!!" to 3 same letters in a row instead of 2

--- app/index.ts
+++ app/index.ts
@@ -236,7 +236,7 @@ class AntiSpam extends Extension {
         to block spam of over 3 consecutive same punctuation
         instead of only blocking over 3 repeated word characters
         */
-        this._currentChatMessage.content = this._currentChatMessage.content.replace(/([a-zA-Z])\1{3,}|([0-9])\2{4,}|([!-\/:-@\[-`{-~])\3{3,}/ig, "$1$1$2$2$2$2$3");
+        this._currentChatMessage.content = this._currentChatMessage.content.replace(/([a-zA-Z])\1{3,}|([0-9])\2{4,}|([!-\/:-@\[-`{-~])\3{3,}/ig, "$1$1$1$2$2$2$2$3");
         return false;
     }
 
