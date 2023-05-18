01e334419cca0734c0c8b4e3f4729488fa11c351
Glasia <132226783+GlasiaVD23@users.noreply.github.com>
2023-05-18 12:12:05 +0200

    feat: Adjust repeated character filter (#1)
    I made the same changes to the TSL repeated character filter as in the TShock repeated characters filter.
    The behaviour should become the same as in TShock (more info, cf https://github.com/popstarfreas/AntiSpam/commit/e70410553c2374724350f99025b90454ed9f1630)
    
    Corrected the trimming behaviour of too many repeated letters: now should trim texts like "Nooooo!!" to "Nooo!!" to 3 same letters in a row instead of 2
    
    Implemented the suggested RegEx by Arthri in https://github.com/popstarfreas/AntiSpam/pull/2#issuecomment-1533533036 to make the repeated character filter also apply on non-ASCII letters
    
    ---------
    
    Co-authored-by: popstarfreas <6698249+popstarfreas@users.noreply.github.com>

--- app/index.ts
+++ app/index.ts
@@ -230,7 +230,14 @@ class AntiSpam extends Extension {
     }
 
     private trimRepeatedLetters(): boolean {
-        this._currentChatMessage.content = this._currentChatMessage.content.replace(/(.)\1{3,}/ig, "$1$1");
+        /*
+        To reach consistency between TSL and TShock the repeated character filter has been adjusted as well
+        to allow 4 same digits in a row ([i:4444] and 9999 pass) and
+        to block spam of over 3 consecutive same punctuation
+        instead of only blocking over 3 repeated word characters
+        Now also blocking consecutive non-ASCII letter spam
+        */
+        this._currentChatMessage.content = this._currentChatMessage.content.replace(/(\d)\1{4,}|([^\d])\2{3,}/ig, "$1$1$1$1$2$2$2");
         return false;
     }
 
